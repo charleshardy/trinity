9417e6d34151e523810017f64811dfe7f7bd206e
le.liu <le.liu@windriver.com>
2025-08-19 14:59:35 +0800

    Fixed issue of "free(): invalid next size (fast)" (Double Freeing Memory)
    This issue often occurs as below.
    
    Trinity 2023.01  Dave Jones <davej@codemonkey.org.uk>
    shm:0x7fa9747a4000-0x7fa9813a0d00 (4 pages)
    [main] Done parsing arguments.
    [main] Initial random seed: 2145030183
    [main] 32-bit syscalls: 1 enabled, 443 disabled.  64-bit syscalls: 1 enabled, 450 disabled.
    [main] Using pid_max = 4194304
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] futex: 0 owner:0 global:1
    [main] Reserved/initialized 100 futexes.
    [main] Generating file descriptors
    [main] Added 84 filenames from /dev
    [main] Added 94619 filenames from /proc
    [main] Added 34537 filenames from /sys
    free(): invalid next size (fast)
    
    Upstream-Status: Pending

--- mm/maps.c
+++ mm/maps.c
@@ -57,6 +57,7 @@ void map_destructor(struct object *obj)
 	map = &obj->map;
 	munmap(map->ptr, map->size);
 	free(map->name);
+	map->name = NULL;
 }
 
 void map_dump(struct object *obj, bool global)
@@ -197,7 +198,9 @@ retry_mmap:
 		retries++;
 		if (retries == 100) {
 			free(obj->map.name);
+			obj->map.name = NULL;
 			free(obj);
+			obj = NULL;
 			return;
 		} else
 			goto retry_mmap;
